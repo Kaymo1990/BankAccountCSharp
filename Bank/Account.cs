a7643e3641930349a0f3138d5eb3be084c884876
Kay <Kay@LAPTOP-R26J0GK6>
2019-09-26 11:52:30 +0100

    updating account class with getter methods

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -6,5 +6,27 @@ namespace Bank
 {
     class Account
     {
+        private float balance;
+        private float minBalance = 50;
+
+        public Account()
+        {
+
+        }
+
+        public Account(int value)
+        {
+            balance = value;
+        }
+
+        public float Balance
+        {
+            get { return balance;  }
+        }
+
+        public float MinBalance
+        {
+            get { return minBalance; }
+        }
     }
 }
